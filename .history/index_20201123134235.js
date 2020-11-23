7378bc9839800e5ed39f91e2008153633cdd0327
lazydevelopr <lazydevelopr@gmail.com>
2020-11-23 13:43:01 +0530

    To test sonarqube after changing jenkins

--- .history/index_20201123134235.js
+++ .history/index_20201123134235.js
@@ -5,7 +5,7 @@ var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
   res.send('hello world to check jenkins and sonarqube');
-  console.log("Successfully connected to sonarqube");
+  console.log("")
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
